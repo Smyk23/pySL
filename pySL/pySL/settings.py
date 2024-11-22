420e152a9e5be4f3568173cb427c4cf73429e0be
Smyk <smyk_ak23@nuwm.edu.ua>
2024-11-22 16:14:58 +0200

    Completd 7th laboratory work

--- pySL/pySL/settings.py
+++ pySL/pySL/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pySL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pySL',
+        'USER': 'postgres',
+        'PASSWORD': 'VariUs7585',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
